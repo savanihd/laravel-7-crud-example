baacbd4845553efbbf6555798195a795ff0bf6f9
Hardik Savani <itsolutionstuff@gmail.com>
2020-03-04 09:08:29 +0530

    added crud example

---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Laravel 7 CRUD Application - ItSolutionStuff.com</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+</head>
+<body style="background-color: #edf7ef">
+  
+<div class="container">
+	<br/>
+	<br/>
+	<br/>
+    @yield('content')
+</div>
+   
+</body>
+</html>
